b170cb132abf3141670a87eb13a94f6a8a510724
srinivas69 <srinivasmail.in@gmail.com>
2022-07-11 17:27:18 +0530

    created new solution for longest common prefix

--- _Problems/longest-common-prefix/app.js
+++ _Problems/longest-common-prefix/app.js
@@ -3,7 +3,8 @@
  * @param {string[]} strs
  * @return {string}
  */
- var longestCommonPrefix = function(strs) {
+// Worng solution
+ var longestCommonPrefix2 = function(strs) {
     
     if(strs.length < 1 || strs.length > 200) {
 
@@ -102,4 +103,98 @@
 
 };
 
-console.log(longestCommonPrefix(["dog","racecar","car"]));
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix2 = function(strs) {
+
+    if(strs.length < 1 || strs.length > 200) {
+
+        return "";
+    }
+
+    const prefixes = new Map();
+    const longestCommonPrefixObj= {};
+
+    for(str of strs){
+
+        const strLen = str.length;
+        console.log(`str: ${str} strLen ${strLen}`);
+
+        if(strLen == 0)
+          continue;
+        for(let i=strLen; i>0; i--){
+
+            //console.log(`i ${i} `);
+            const prefixOfStr = str.substring(0,i);
+
+            console.log(`prefixOfStr ${prefixOfStr}`);
+
+            const prefixStrCount = prefixes.get(prefixOfStr);
+            let prefixStrCountNew = 1;
+            if(prefixStrCount) {
+
+                prefixStrCountNew = prefixStrCount+1;
+                prefixes.set(prefixOfStr,prefixStrCountNew);
+            } else {
+
+                prefixes.set(prefixOfStr,prefixStrCountNew);
+            }
+
+            const lastLongestPrefixWord = longestCommonPrefixObj.word;
+                const lastLongestPrefixWordCount = longestCommonPrefixObj.count;
+
+                if(lastLongestPrefixWord) {
+
+                    if(prefixStrCountNew > lastLongestPrefixWordCount){
+
+                        longestCommonPrefixObj.word = prefixOfStr;
+                        longestCommonPrefixObj.count = prefixStrCountNew;
+                    }
+                    
+                } else {
+
+                    longestCommonPrefixObj.word = prefixOfStr;
+                    longestCommonPrefixObj.count = prefixes.get(prefixOfStr);
+                }
+        }
+    }
+
+    console.log("prefixes: ",prefixes);
+    console.log("longestCommonPrefixObj: ",longestCommonPrefixObj);
+
+    if(!longestCommonPrefixObj.count || (strs.length>1 && longestCommonPrefixObj.count == 1)){
+        return "";
+    } else
+        return longestCommonPrefixObj.word;
+ }
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let maxLength = -Infinity;
+    let str = '';
+    for(let i = 0; i < strs[0].length; i++) {
+        let end = i;
+        let compare = strs[0].split('').splice(0, (end + 1)).join('');
+        console.log("compare, ",compare)
+        let isInclude = true;
+        strs.forEach(el => {
+            if(el.indexOf(compare) !== 0){
+                isInclude = false;
+            }
+        });
+        if(isInclude && compare.length > maxLength){
+            maxLength = compare.length;
+            str = compare;
+        }
+        
+    }
+    return str;
+};
+
+console.log(longestCommonPrefix(["reflower","flow","flight"]));
