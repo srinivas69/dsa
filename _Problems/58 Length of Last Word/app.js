afee34934921303472edfaf3c4caa092a2e2fac2
srinivas69 <srinivasmail.in@gmail.com>
2022-07-14 15:32:01 +0530

    created solution for length of last word

---
+++ _Problems/58 Length of Last Word/app.js
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/length-of-last-word 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLastWord = function(s) {
+    
+    const sLen = s.length;
+    console.log(sLen);
+    if(sLen<1 || sLen>10*10*10*10) 
+       return 0;
+
+    let rightPointer = sLen;
+    let lastWord = "";
+    //console.log("s: ",s," sLen: ",sLen);
+    for(let i=sLen-1; i>=0; i--) {
+
+        console.log("for s[i]: ",s[i]);
+        if(!lastWord && (/\s/).test(s[i])) {
+
+            rightPointer--;
+            continue;
+        }
+
+        if((/\s/).test(s[i])) {
+
+            return lastWord.length;
+        } else {
+            lastWord = lastWord+s[i];
+        }
+    }
+
+    return lastWord.length;
+};
+
+console.log(lengthOfLastWord("a"));
