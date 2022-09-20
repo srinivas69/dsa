75bec5f2ba59cba87138f20331a1c0b6419f4f9f
srinivas69 <srinivasmail.in@gmail.com>
2022-09-20 11:21:36 +0530

    created solution for 104

--- _Problems/104. Maximum Depth of Binary Tree/app.js
+++ _Problems/104. Maximum Depth of Binary Tree/app.js
@@ -11,7 +11,7 @@
  * @param {TreeNode} root
  * @return {number}
  */
- var maxDepth = function(root) {
+ var maxDepth1 = function(root) {
 
     if(!root) {
         return 0;
@@ -26,8 +26,8 @@
     let currentLeft = root.left ? root.left : null;
     let currentRight = root.right ? root.right : null;
 
-    const leftDepth = currentLeft != null ? findDepth(currentLeft,depth) : 1;
-    const rightDepth = currentRight != null ? findDepth(currentRight,depth) : 1;
+    const leftDepth = currentLeft != null ? findDepth1(currentLeft,depth) : 1;
+    const rightDepth = currentRight != null ? findDepth1(currentRight,depth) : 1;
 
     console.log(`leftDepth ${leftDepth} rightDepth ${rightDepth}`);
 
@@ -36,24 +36,94 @@
     else 
        return rightDepth;
 };
+// Solution 1
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxDepth = function(root) {
 
-function findDepth(node,depth) {
+    if(!root) {
+        return 0;
+    }
+    
+    if(root && !root.left && !root.right) {
+        return 1;
+    }
+
+    let initDepth = 0;
+    let maxDepth = 0;
+
+    (
+       function findDepth(node, depth) {
+
+            //console.log(node, depth, maxDepth);
+            if(node) {
+                depth++;
+                if(depth > maxDepth) {
+                    maxDepth = depth;
+                }
+                findDepth(node.left,depth);
+                findDepth(node.right,depth);
+            }
+        
+            //return maxDepth;
+         }
+    )(root,initDepth);
+
+    //console.log(maxDepth);
+
+    return maxDepth;
+ }
+
+ function findDepth(node,depth,maxDepth) {
+
+    console.log(node, depth, maxDepth);
+    if(node) {
+        depth++;
+        if(depth > maxDepth) {
+            maxDepth = depth;
+        }
+        findDepth(node.left, depth, maxDepth);
+        findDepth(node.right, depth, maxDepth);
+    }
+
+    return maxDepth;
+ }
+
+ // Solution 2
+ /**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    
+    if(!root){
+       return 0;
+   }
+   let lh = maxDepth(root.left);
+   let rh = maxDepth(root.right);
+   
+   return 1+Math.max(lh,rh);
+}
+
+function findDepth1(node,depth) {
 
     let currentNode;
     depth++;
     if(node.left && node.right) {
 
-        findDepth(node.left,depth);
-        findDepth(node.right,depth);
+        findDepth1(node.left,depth);
+        findDepth1(node.right,depth);
 
     } else if(node.left) {
 
         currentNode = node.left;
-        return findDepth.call(this,currentNode,depth);
+        return findDepth1.call(this,currentNode,depth);
     } else if(node.right) {
 
         currentNode = node.right;
-        return findDepth.call(this,currentNode,depth);
+        return findDepth1.call(this,currentNode,depth);
     } else {
         return depth;
     }
